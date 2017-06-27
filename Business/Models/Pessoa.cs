fd28afbfaeb68d5285ca9f01c80d834d69daa1d3
creis <cristiano@prion.com.br>
2017-06-27 08:50:22 -0300

    Initial Commit

---
+++ Business/Models/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Models
+{
+    public class Pessoa
+    {
+        public long IdPessoa { get; set; }
+        public String Nome { get; set; }
+        public String CPF { get; set; }
+    }
+}
