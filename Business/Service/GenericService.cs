28a32483aef521d6c4167c2c8a32e6a2b24287e1
cristiano2005reis <cristianoreisnascimento@gmail.com>
2017-07-13 21:20:25 -0300

    Criação GenericService

---
+++ Business/Service/GenericService.cs
@@ -0,0 +1,20 @@
+﻿using Business.Context;
+using Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Service
+{
+    public class GenericService<T>: IDisposable where T: GenericModel
+    {
+        private CadastroContext context;
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
